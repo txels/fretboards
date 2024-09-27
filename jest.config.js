e72d26f4bacd40e625043105b74769e3fe9c4b84
Carles Barrobés <carles@barrobes.com>
2024-09-27 08:17:04 +0200

    Remove vue-cli plugin dependency

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  verbose: true,
-  transform: {
-    "\\.[jt]sx?$": "babel-jest",
+  //preset: "@vue/cli-plugin-unit-jest",
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
+  testMatch: [
+    '**/tests/unit/**/*.spec.[jt]s?(x)',
+    '**/__tests__/*.[jt]s?(x)'
+  ], verbose: true,
+  transform: {
+     '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$': require.resolve('jest-transform-stub'),
+    '^.+\\.jsx?$': require.resolve('babel-jest'),
+  },
+  transformIgnorePatterns: ['/node_modules/'],
 };
